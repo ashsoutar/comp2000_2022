d8ab63e7900a33cc1e377a8acd8c66b6e2258a7e
ashsoutar <100192633+ashsoutar@users.noreply.github.com>
2022-08-28 22:09:04 +1000

    updated cell block

--- src/Cell.java
+++ src/Cell.java
@@ -3,9 +3,9 @@ import java.awt.*;
 class Cell extends Rectangle {
 
 static int size = 35;
-Tile tile;
+GameTile tile;
 
-public Cell(int x, int y, Tile tile) {
+public Cell(int x, int y, GameTile tile) {
 super(x, y, size, size);
 this.tile = tile;
 }
@@ -13,8 +13,25 @@ this.tile = tile;
 // Draw the tile
 void paint(Graphics g) {
 tile.paint(g, this);
+g.setColor(Color.WHITE);
 }
+public void paint(Graphics g, Point mousePos) {
+    if(contains(mousePos)) {
+      g.setColor(Color.GRAY);
+    } else {
+      g.setColor(Color.WHITE);
+    }
+    g.fillRect(x, y, size, size);
+    g.setColor(Color.BLACK);
+    g.drawRect(x, y, size, size);
+  }
+
+  public boolean contains(Point p) {
+    if(p != null) {
+      return x < p.x && x+size > p.x && y < p.y && y+size > p.y;
+    } else {
+      return false;
+    }
 
 }
-
-
+}
